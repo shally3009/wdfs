17508c95db5399c316fb92a6555407b0d13ee7ff
shally3009 <mittalshally30@gmail.com>
2025-01-21 15:35:13 +0530

    done

---
+++ OneDrive/Desktop/Ecommerce-Follow-Along/my-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Route,Routes } from 'react-router-dom'
+import {Login} from './components/login'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
